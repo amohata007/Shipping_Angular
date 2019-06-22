d8ba246c6e4f25ef5a2730122f7dcbcb5822b339
Abhishek mohata <amohata.007@gmail.com>
2019-06-22 07:52:36 +0530

    first one

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbCollapseModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  schemas: [ NO_ERRORS_SCHEMA ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
