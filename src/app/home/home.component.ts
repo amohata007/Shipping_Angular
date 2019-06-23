d1a0cd5dbfc36efba4af830e440a0a39b646fa81
Abhishek mohata <amohata.007@gmail.com>
2019-06-23 08:33:08 +0530

    some changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,17 +11,56 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
  public ngOnInit() {
-    $('.counter').each(function () {
-      $(this).prop('Counter',0).animate({
-          Counter: $(this).text()
-      }, {
-          duration: 4000,
-          easing: 'swing',
-          step: function (now) {
-              $(this).text(Math.ceil(now));
-          }
+
+  var a = 0;
+  $(window).scroll(function() {
+
+    var oTop = $('#cntr').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+      $('.counter').each(function() {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+            countNum: countTo
+          },
+
+          {
+
+            duration: 4000,
+            easing: 'swing',
+            step: function() {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+              $this.text(this.countNum);
+              //alert('finished');
+            }
+
+          });
       });
+      a = 1;
+    }
+
   });
+
+
+
+
+  //   $('.counter').each(function () {
+  //     $(this).prop('Counter',0).animate({
+  //         Counter: $(this).text()
+  //     }, {
+  //         duration: 4000,
+  //         easing: 'swing',
+  //         step: function (now) {
+  //             $(this).text(Math.ceil(now));
+  //         }
+  //     });
+  // });
+
+
   }
 
 }
