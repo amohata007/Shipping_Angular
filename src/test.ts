ccc9099544393cfa8906f840b8a3bf72c469f07f
Abhishek mohata <36244104+amohata007@users.noreply.github.com>
2019-08-02 07:50:29 +0530

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -17,5 +17,5 @@ getTestBed().initTestEnvironment(
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
-context.keys().map(context);
+context.keys().map(context); 
  
